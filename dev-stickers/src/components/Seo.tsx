950e798db6bafaa6559b5dcd39a9579a1c1f3469
bae-sh <hyeon1293@gmail.com>
2023-03-20 12:56:24 +0900

    #10.9 useStaticQuery

--- dev-stickers/src/components/Seo.tsx
+++ dev-stickers/src/components/Seo.tsx
@@ -1,7 +1,22 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 interface ISeo {
   title: string;
 }
 export default function Seo({ title }: ISeo) {
-  return <title>{`${title} | DevStickers`}</title>;
+  const data = useStaticQuery<Queries.SeoQueryQuery>(graphql`
+    query SeoQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  console.log(data.site?.siteMetadata?.title);
+  return (
+    <title>
+      {title} | {data.site?.siteMetadata?.title}
+    </title>
+  );
 }
