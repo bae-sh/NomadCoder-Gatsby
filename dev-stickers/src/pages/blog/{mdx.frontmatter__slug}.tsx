e99eac2db1c592f700405163dc08a2f1f7120fd0
bae-sh <hyeon1293@gmail.com>
2023-03-22 02:06:29 +0900

    #10.13 Blog Post

--- dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-
-export default function BlogPost() {
+import { graphql } from 'gatsby';
+import Seo from '../../components/Seo';
+interface IBlogPostProps {
+  data: Queries.PorstDetailQuery;
+  children: any;
+}
+export default function BlogPost({ data, children }: IBlogPostProps) {
+  console.log(data, children);
   return (
     <Layout title="Blog Post">
-      <div />
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PorstDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => <Seo title={data.mdx?.frontmatter?.title!} />;
