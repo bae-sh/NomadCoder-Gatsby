34c8374994486eaec9ac8c3775f1887f9f990add
bae-sh <hyeon1293@gmail.com>
2023-03-22 16:12:02 +0900

    #10.15 GatsbyImage

--- dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import { graphql } from 'gatsby';
 import Seo from '../../components/Seo';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 interface IBlogPostProps {
   data: Queries.PorstDetailQuery;
   children: any;
 }
 export default function BlogPost({ data, children }: IBlogPostProps) {
-  console.log(data, children);
+  const image = getImage(data.mdx?.frontmatter?.headerimage?.childImageSharp?.gatsbyImageData!);
   return (
     <Layout title="Blog Post">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -18,13 +20,17 @@ export default function BlogPost({ data, children }: IBlogPostProps) {
 export const query = graphql`
   query PorstDetail($frontmatter__slug: String) {
     mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
-      body
       frontmatter {
         author
         category
         date
         slug
         title
+        headerimage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
